cfd96c00b4b62d46b2f431d51ed565783d439839
sambattalio <sambattalio99@gmail.com>
2020-01-26 13:31:08 -0500

    works, doesn't check argument count

---
+++ src/head.zig
@@ -0,0 +1,35 @@
+const std  = @import("std");
+const File = std.fs.File;
+
+
+const BUFSIZ: u16 = 256;
+
+// Reads first n lines of file and
+// prints to stdout
+pub fn head(n: u32, path: []const u8) !void {
+    const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    const stream = &file.inStream().stream;
+
+    var buffer: [BUFSIZ]u8 = undefined;
+    var i: u8 = 0;
+
+    while (try stream.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        std.debug.warn("{}\n", .{line});
+        i += 1;
+        if (i >= n) break;
+    }
+
+
+}
+
+pub fn main() anyerror!void {
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    defer std.process.argsFree(std.heap.page_allocator, args);
+
+    const n = try std.fmt.parseInt(u32, args[1], 10);
+
+    try head(n, "test");
+
+}
