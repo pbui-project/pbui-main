7f623dbf8c1fee33e24e8b1bdd7aae99b72ba09d
sambattalio <sambattalio99@gmail.com>
2020-02-04 20:31:02 -0500

    false added

---
+++ src/false.zig
@@ -0,0 +1,11 @@
+// Return 1... wrapped in function in
+// case other debugging stuff needed
+pub fn return_false() anyerror!u8 {
+    return 1;
+}
+
+pub fn main() anyerror!u8 {
+    return return_false() catch |err| {
+        return err;
+    };
+}
