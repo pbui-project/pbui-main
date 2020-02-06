03d476b29deda1609b3fe63db117415f1e862bf7
Sam Battalio <sambattalio99@gmail.com>
2020-02-06 16:15:05 -0500

    initial grab of sleep (#10)

---
+++ src/sleep.zig
@@ -0,0 +1,36 @@
+// sleep.zig
+// nap time bb
+
+const std = @import("std");
+const stdout = &std.io.getStdOut().outStream().stream;
+
+pub fn sleep(n: u32) anyerror!void {
+    // TODO: docs say maybe spurious wakeups but no way to tell.. possible PR or issue maybe?
+    std.os.nanosleep(n, 0);
+}
+
+pub fn main() anyerror!u8 {
+    // out of memory panic
+    const args = std.process.argsAlloc(std.heap.page_allocator) catch |err| {
+        try stdout.print("Out of memory: {}\n", .{err});
+        return 1;
+    };
+    defer std.process.argsFree(std.heap.page_allocator, args);
+
+    // check len of args
+    if (args.len != 2) {
+        try stdout.print("usage: ./sleep n\n", .{});
+        return 1;
+    }
+
+    // must be a number
+    const n = std.fmt.parseInt(u32, args[1], 10) catch |err| {
+        try stdout.print("Error: sleep arg must be a number!\n", .{});
+        return 1;
+    };
+
+    try sleep(n);
+
+    // exit success
+    return 0;
+}
