3a54db29882120f6316b7e50273fec0b213c6cf4
Kyle Weingartner <kyle@weingartner.xyz>
2020-02-12 18:12:13 -0500

    basename: added flags, (hopefully) fully working

--- src/basename.zig
+++ src/basename.zig
@@ -1,21 +1,65 @@
 const std = @import("std");
+const opt = @import("opt.zig");
 const stdout = &std.io.getStdOut().outStream().stream;
+const warn = std.debug.warn;
 
 // TODO deal with suffix parsing and stuff with arguments
-pub fn basename(paths: [][]const u8, zero: bool, no_suffix: bool) !void {
+pub fn basename(path: []const u8, terminator: u8, suffix: ?[]u8) !void {
     // basename calls basenameposix if not windows... more robust
     // to just use basename
-    const terminator: u8 = if (zero) '\x00' else '\n';
     // loop through paths and call dirname
-    var i: usize = 0;
-    while (i < paths.len) : (i += 1) {
-        var name = std.fs.path.basename(paths[i]);
 
-        // TODO suffix handling
-
-        try stdout.print("{}{c}", .{ name, terminator });
+    // GNU basename does this.  I wonder if it works the same way on Windows.
+    if (path.len == 1 and path[0] == '/') {
+        try stdout.print("/{c}", .{terminator});
+        return;
     }
+    const name = std.fs.path.basename(path);
+    var stripped: [*]u8 = undefined;
+    if (suffix) |suf| {
+        if (std.mem.eql(u8, name[name.len - suf.len ..], suf)) {
+            try stdout.print("{}{c}", .{ name[0 .. name.len - suf.len], terminator });
+            return;
+        }
+    }
+    try stdout.print("{}{c}", .{ name, terminator });
 }
+
+const BasenameFlags = enum {
+    Multiple,
+    Suffix,
+    Zero,
+    Help,
+    Version,
+};
+
+var flags = [_]opt.Flag(BasenameFlags){
+    .{
+        .name = BasenameFlags.Help,
+        .long = "help",
+    },
+    .{
+        .name = BasenameFlags.Version,
+        .long = "version",
+    },
+    .{
+        .name = BasenameFlags.Multiple,
+        .short = 'a',
+        .long = "multiple",
+    },
+    .{
+        .name = BasenameFlags.Suffix,
+        .short = 's',
+        .long = "suffix",
+        .mandatory = true,
+        .kind = opt.ArgTypeTag.String,
+    },
+    .{
+        .name = BasenameFlags.Zero,
+        .short = 'z',
+        .long = "zero",
+    },
+};
 
 pub fn main() !void {
     // out of memory panic
@@ -25,12 +69,60 @@ pub fn main() !void {
     };
     defer std.process.argsFree(std.heap.page_allocator, args);
 
+    var multiple: bool = false;
+    var eolchar: u8 = '\n';
+    var suffix: ?[]u8 = null;
     // check len of args
-    if (args.len != 2) {
-        try stdout.print("usage: ./basename FILENAME\n", .{});
+    // if (args.len != 2) {
+    //     try stdout.print("usage: ./basename FILENAME\n", .{});
+    //     return;
+    // }
+
+    var it = opt.FlagIterator(BasenameFlags).init(flags[0..], args);
+    while (it.next_flag() catch {
         return;
+    }) |flag| {
+        switch (flag.name) {
+            BasenameFlags.Help => {
+                warn("(help screen here)\n", .{});
+                return;
+            },
+            BasenameFlags.Version => {
+                warn("(version info here)\n", .{});
+                return;
+            },
+            BasenameFlags.Multiple => {
+                multiple = true;
+            },
+            BasenameFlags.Suffix => {
+                multiple = true;
+                suffix = flag.value.String.?;
+            },
+            BasenameFlags.Zero => {
+                eolchar = '\x00';
+            },
+        }
     }
 
-    // run command
-    try basename(args[1..], false, false);
+    const first_maybe = it.next_arg();
+    if (first_maybe) |first| {
+        if (!multiple and suffix == null) {
+            suffix = it.next_arg();
+            if (it.next_arg()) |arg| {
+                warn("{}: extra operand '{}'\n", .{ args[0], arg });
+                warn("Try '{} --help' for more information.\n", .{args[0]});
+                return;
+            }
+        }
+        try basename(first, eolchar, suffix);
+        if (multiple) {
+            while (it.next_arg()) |arg| {
+                try basename(arg, eolchar, suffix);
+            }
+        }
+    } else {
+        warn("{}: missing operand.\n", .{args[0]});
+        warn("Try '{} --help' for more information.\n", .{args[0]});
+        return;
+    }
 }
