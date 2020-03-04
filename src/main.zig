22bd4a8c23881fb370534b43f8ac81429759eff2
Zephan M. Enciso <zmenciso@gmail.com>
2020-03-03 22:38:51 -0500

    Main flags and cleanup

--- src/main.zig
+++ src/main.zig
@@ -17,16 +17,6 @@ const testing = std.testing;
 
 const mainFlags = enum {
     Help,
-    Basename,
-    Dirname,
-    False,
-    Head,
-    LS,
-    MKDir,
-    RM,
-    Sleep,
-    Tail,
-    True,
 };
 
 var flags = [_]opt.Flag(mainFlags){
@@ -34,46 +24,6 @@ var flags = [_]opt.Flag(mainFlags){
         .name = mainFlags.Help,
         .short = 'h',
         .long = "help",
-    },
-    .{
-        .name = mainFlags.Basename,
-        .long = "basename",
-    },
-    .{
-        .name = mainFlags.Dirname,
-        .long = "dirname",
-    },
-    .{
-        .name = mainFlags.False,
-        .long = "false",
-    },
-    .{
-        .name = mainFlags.Head,
-        .long = "head",
-    },
-    .{
-        .name = mainFlags.LS,
-        .long = "ls",
-    },
-    .{
-        .name = mainFlags.MKDir,
-        .long = "mkdir",
-    },
-    .{
-        .name = mainFlags.RM,
-        .long = "rm",
-    },
-    .{
-        .name = mainFlags.Sleep,
-        .long = "sleep",
-    },
-    .{
-        .name = mainFlags.Tail,
-        .long = "tail",
-    },
-    .{
-        .name = mainFlags.True,
-        .long = "true",
     },
 };
 
@@ -102,43 +52,45 @@ pub fn main() anyerror!u8 {
 
     var it = opt.FlagIterator(mainFlags).init(flags[0..], args);
 
-    // TODO CHECK FOR HELP OR VERSION FLAG
+    while (it.next_flag() catch {
+        return r;
+    }) |flag| {
+        switch (flag.name) {
+            mainFlags.Help => {
+                return usage(); 
+            },
 
-    // HAS to be ./pbui APPLET_NAME jaklsfjljdklsafjldasjlfalkdsjf
+            else => {
+                return usage();
+            },
+        }
+    }
+
+    // HAS to be ./pbui APPLET_NAME [ARGS]
     var applet_name = it.next_arg().?;
 
     if (std.mem.eql(u8, applet_name, "basename")) {
-        warn("Call to basename (missing arguments)\n", .{});
         return basename.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "false")) {
-        warn("Call to false (missing arguments)\n", .{});
         return fls.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "dirname")) {
-        warn("Call to dirname (missing arguments)\n", .{});
         return dirname.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "head")) {
-        warn("Call to head (missing arguments)\n", .{});
         return head.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "ls")) {
-        warn("Call to ls (missing arguments)\n", .{});
         return ls.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "mkdir")) {
-        warn("Call to mkdir (missing arguments)\n", .{});
         return mkdir.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "rm")) {
-        warn("Call to rm (missing arguments)\n", .{});
         return rm.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "sleep")) {
-        warn("Call to sleep (missing arguments)\n", .{});
         return sleep.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "tail")) {
-        warn("Call to tail (missing arguments)\n", .{});
         return tail.main(it.argv[1..]);
     } else if (std.mem.eql(u8, applet_name, "true")) {
-        warn("Call to true (missing arguments)\n", .{});
         return tru.main(it.argv[1..]);
     } else {
-        warn("Bleh", .{});
+        return usage();
     }
     return r;
 }
