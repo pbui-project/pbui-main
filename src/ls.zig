e31795af400a24739094032ac65a8857dd6b0c94
Jacques Charboneau <jcharbon@nd.edu>
2020-04-14 16:12:32 -0400

    Added flags to ls; in the process of readjusting the flag properties to zig 0.6.0

--- src/ls.zig
+++ src/ls.zig
@@ -1,4 +1,37 @@
 const std = @import("std");
+const opt = @import("opt.zig");
+
+var ALPHA : bool = false;
+var RECUR : bool = false;
+var ALL   : bool = false;
+
+const lsFlags = enum {
+    All,
+    Alpha,
+    Recursive,
+    Help,
+};
+
+var flags = [_]opt.Flag(lsFlags){
+    .{
+        .name = lsFlags.Help,
+        .long = "help",
+    },
+    .{
+        .name = lsFlags.All,
+        .short = 'a',
+    },
+    .{
+        .name = lsFlags.Recursive,
+        .short = 'r',
+    },
+    .{
+        .name = lsFlags.Alpha,
+        .short = 'A',
+        .long = "Alpha",
+    },
+};
+
 
 fn show_file(path: []const u8) void {
     std.debug.warn("{}\n", .{path});
@@ -10,17 +43,25 @@ fn show_directory(path: []const u8) !void {
 
     const allocator = &arena.allocator;
 
-    var dents = std.ArrayList([]const u8).init(allocator);
+    var dents = std.ArrayList(std.fs.Dir.Entry).init(allocator);
     defer dents.deinit();
 
-    if (std.fs.cwd().openDirList(path)) |d_opened| {
+    if (std.fs.cwd().openDir(path, std.fs.Dir.OpenDirOptions{ .access_sub_paths = true, .iterate = true })) |d_opened| {
         var dir = d_opened;
         var iter = dir.iterate();
         while (try iter.next()) |entry| {
-            try dents.append(entry.name);
+            try dents.append(entry);
         }
-        for (dents.toSlice()) |entry| {
-            std.debug.warn("{}\n", .{entry});
+        if(ALL == true) std.debug.warn(".\n..\n", .{});
+        for (dents.items) |entry| {
+            if(ALL == true or entry.name[0] != '.') {
+                if(entry.kind == std.fs.Dir.Entry.Kind.Directory){ 
+                    std.debug.warn("{}/\n", .{entry.name});   
+                }
+                else {
+                    std.debug.warn("{}\n", .{entry.name});
+                }
+            }
         }
         std.fs.Dir.close(&dir);
     } else |err| {
@@ -31,12 +72,34 @@ fn show_directory(path: []const u8) !void {
 }
 
 pub fn main(args: [][]u8) anyerror!u8 {
+    var it = opt.FlagIterator(lsFlags).init(flags[0..], args);
+    while(it.next_flag() catch { return 1; }) |flag| {
+        switch (flag.name){
+            lsFlags.Help => {
+                std.debug.warn("Usgae: ls FLAGS DIRECTORIES\n", .{});
+                return 1;
+            },
+            lsFlags.All => {
+                ALL = true;
+            },
+            lsFlags.Recursive => {
+                RECUR = true;
+            },
+            lsFlags.Alpha => {
+                ALPHA = true;
+            },
+        }
+    }
 
-    if (args.len > 1) {
-        for (args) |arg, i| {
-            if (i != 0) {
-                const result = show_directory(arg);
-            }
+    var dirs = std.ArrayList([]u8).init(std.heap.page_allocator);
+    while (it.next_arg()) |direntry| {
+        try dirs.append(direntry);
+    }
+
+
+    if (dirs.items.len > 0) {
+        for (dirs.items) |arg| {
+            const result = show_directory(arg);
         }
     } else const result = show_directory(".");
 
