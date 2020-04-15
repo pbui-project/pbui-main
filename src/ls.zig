4ef894faff4b3e78a7d96b404ede0e22492a3a62
Jacques Charboneau <jcharbon@nd.edu>
2020-04-15 00:10:31 -0400

    ls with alphabetical ordering and 'all' option

--- src/ls.zig
+++ src/ls.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 const opt = @import("opt.zig");
 
-var ALPHA : bool = false;
-var RECUR : bool = false;
-var ALL   : bool = false;
+var ALPHA: bool = false;
+var RECUR: bool = false;
+var ALL: bool = false;
 
 const lsFlags = enum {
     All,
@@ -32,6 +32,82 @@ var flags = [_]opt.Flag(lsFlags){
     },
 };
 
+pub fn compare_words(word1: []const u8, word2: []const u8) bool {
+    var maxlen: usize = 0;
+    var w: bool = false;
+    var index: usize = 0;
+    if (word1.len > word2.len) {
+        maxlen = word2.len;
+    } else {
+        maxlen = word1.len;
+        w = true;
+    }
+
+    while (maxlen > 0) {
+        var val1: usize = word1[index];
+        var val2: usize = word2[index];
+
+        if (val1 > 96 and val1 < 123) {
+            val1 = val1 - 32;
+        }
+        if (val2 > 96 and val2 < 123) {
+            val2 = val2 - 32;
+        }
+
+        if (val1 < val2) {
+            return true;
+        }
+        if (val1 > val2) {
+            return false;
+        }
+
+        index = index + 1;
+        maxlen = maxlen - 1;
+    }
+
+    return w;
+}
+
+pub fn alpha_ArrayList(oldList_: std.ArrayList(std.fs.Dir.Entry)) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = &arena.allocator;
+
+    var newList = std.ArrayList(std.fs.Dir.Entry).init(allocator);
+    var oldList = oldList_;
+    defer newList.deinit();
+
+    var nextpos: usize = 0;
+    var nextword: []const u8 = "";
+
+    while (oldList.items.len > 0) {
+        nextword = oldList.items[0].name;
+        var word: usize = oldList.items.len - 1;
+
+        while (word > 0) {
+            if (compare_words(nextword, oldList.items[word].name) == false) {
+                nextword = oldList.items[word].name;
+                nextpos = word;
+            }
+            word = word - 1;
+        }
+        const ret = newList.append(oldList.swapRemove(nextpos));
+        nextpos = 0;
+    }
+
+    for (newList.items) |entry| {
+        if (ALL == true or entry.name[0] != '.') {
+            if (entry.kind == std.fs.Dir.Entry.Kind.Directory) {
+                std.debug.warn("{}/\n", .{entry.name});
+            } else {
+                std.debug.warn("{}\n", .{entry.name});
+            }
+        }
+    }
+
+    return;
+}
 
 fn show_file(path: []const u8) void {
     std.debug.warn("{}\n", .{path});
@@ -52,29 +128,34 @@ fn show_directory(path: []const u8) !void {
         while (try iter.next()) |entry| {
             try dents.append(entry);
         }
-        if(ALL == true) std.debug.warn(".\n..\n", .{});
-        for (dents.items) |entry| {
-            if(ALL == true or entry.name[0] != '.') {
-                if(entry.kind == std.fs.Dir.Entry.Kind.Directory){ 
-                    std.debug.warn("{}/\n", .{entry.name});   
-                }
-                else {
-                    std.debug.warn("{}\n", .{entry.name});
+
+        if (ALL == true) std.debug.warn(".\n..\n", .{});
+        if (ALPHA == true) {
+            const ret = alpha_ArrayList(dents);
+        } else {
+            for (dents.items) |entry| {
+                if (ALL == true or entry.name[0] != '.') {
+                    if (entry.kind == std.fs.Dir.Entry.Kind.Directory) {
+                        std.debug.warn("{}/\n", .{entry.name});
+                    } else {
+                        std.debug.warn("{}\n", .{entry.name});
+                    }
                 }
             }
         }
         std.fs.Dir.close(&dir);
     } else |err| {
-        if(err == error.NotDir) show_file(path);
+        if (err == error.NotDir) show_file(path);
         return;
     }
-
 }
 
 pub fn main(args: [][]u8) anyerror!u8 {
     var it = opt.FlagIterator(lsFlags).init(flags[0..], args);
-    while(it.next_flag() catch { return 1; }) |flag| {
-        switch (flag.name){
+    while (it.next_flag() catch {
+        return 1;
+    }) |flag| {
+        switch (flag.name) {
             lsFlags.Help => {
                 std.debug.warn("Usgae: ls FLAGS DIRECTORIES\n", .{});
                 return 1;
@@ -96,13 +177,13 @@ pub fn main(args: [][]u8) anyerror!u8 {
         try dirs.append(direntry);
     }
 
-
     if (dirs.items.len > 0) {
         for (dirs.items) |arg| {
             const result = show_directory(arg);
         }
-    } else const result = show_directory(".");
+    } else {
+        const result = show_directory(".");
+    }
 
     return 0;
-
 }
