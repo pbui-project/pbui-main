19fb3b0afc4fa9b7c64407a441f737c96cb1c821
Dane Williams <dwilli36@nd.edu>
2020-04-29 22:20:42 -0700

    gets pwd logical dir

--- src/pwd.zig
+++ src/pwd.zig
@@ -1,8 +1,14 @@
 const std = @import("std");
 
 pub fn main(args: [][]u8) anyerror!u8 {
+
+    //read $PWD to get pwd including symlinks
+    const pwd = std.os.getenv("PWD");
+
+
+    // send pwd to stdout
     const stdout = std.io.getStdOut().outStream();
-    try stdout.print("Hello, {}!\n", .{"zig"});
+    try stdout.print("{}\n", .{pwd});
 
     return 0;
 }
